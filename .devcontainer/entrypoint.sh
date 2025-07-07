82db17785c505eeae2662993d372d3b58f409111
yonwang <Yongjing.Wang@UGent.be>
2025-07-07 19:14:00 +0200

    temp changes

--- .devcontainer/entrypoint.sh
+++ .devcontainer/entrypoint.sh
@@ -2,13 +2,16 @@
 # shellcheck disable=SC2086
 set -e
 
-USERNAME=${USERNAME:-ros_user}
+USERNAME=${USERNAME:-rdev}
 USER_ID=$(id -u ${USERNAME})
 
-if [ "${USER_ID}" != "$(id -u)" ]; then
-    echo "Changing ${USERNAME} user ID to match your host's user ID (${USER_ID})"
-    usermod --uid ${USER_ID} ${USERNAME}
-    find /home/${USERNAME} -user "$(id -u)" -exec chown -h ${USER_ID} {} +
+if [ "$(uname -s)" != "Darwin" ]; then
+
+    if [ "${USER_ID}" != "$(id -u)" ]; then
+        echo "Changing ${USERNAME} user ID to match your host's user ID (${USER_ID})"
+        usermod --uid ${USER_ID} ${USERNAME}
+        find /home/${USERNAME} -user "$(id -u)" -exec chown -h ${USER_ID} {} +
+    fi
 fi
 
 cd /home/${USERNAME}
